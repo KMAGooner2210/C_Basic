fb411263fa8e9493a3a0a403abe11681db8169db
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:16:49 +0700

    Update Untitled6.c

--- Part03_Con_Tro/Ex6/Untitled6.c
+++ Part03_Con_Tro/Ex6/Untitled6.c
@@ -1,3 +1,9 @@
+//**Bài 6: Sao chép chuỗi bằng con trỏ** 
+//•	Yêu cầu: Viết hàm sao chép chuỗi bằng con trỏ, không dùng strcpy.
+//•	Hướng dẫn: 
+//o	Chạy và kiểm tra kết quả (Chuỗi sao chép: Hello).
+//Thử sao chép chuỗi dài hơn (nhưng đảm bảo dest đủ lớn).
+
 #include<stdio.h>
 void copy_string(char *dest, const char *src){
 	while(*src != '\0'){
