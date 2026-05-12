e4eb35a359368a642cdb9031be7dea1f1dd21f0a
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:15:22 +0700

    Update Untitled4.c

--- Part03_Con_Tro/Ex4/Untitled4.c
+++ Part03_Con_Tro/Ex4/Untitled4.c
@@ -1,3 +1,11 @@
+//**Bài 4: In từng ký tự của chuỗi bằng con trỏ**
+//•	Yêu cầu: Viết chương trình khai báo chuỗi (dùng mảng ký tự hoặc chuỗi hằng), dùng con trỏ để in từng ký tự của chuỗi.
+//•	Mã nguồn mẫu: 
+//•	Hướng dẫn: 
+//o	Chạy và kiểm tra đầu ra (H e l l o).
+//o	Thử sửa đổi một ký tự (ví dụ: *ptr = 'h'; với str[]) và in lại chuỗi.
+//o	Thử dùng chuỗi hằng (char *str = "Hello";) và giải thích tại sao không sửa được.
+
 #include<stdio.h>
 int main(){
 	char str[]="Gooner";
