97a7a2952f3fe0c33521608b24371ae8cd55d43b
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:18:10 +0700

    Update Untitled8.c

--- Part03_Con_Tro/Ex8/Untitled8.c
+++ Part03_Con_Tro/Ex8/Untitled8.c
@@ -1,3 +1,10 @@
+//**Bài 8: Kiểm tra lỗi con trỏ NULL** 
+//•	Yêu cầu: Viết chương trình khởi tạo con trỏ NULL, kiểm tra trước khi giải tham chiếu để tránh lỗi.
+//•	Hướng dẫn: 
+//o	Chạy và kiểm tra đầu ra.
+//o	Thử bỏ điều kiện kiểm tra NULL và giải tham chiếu *ptr để quan sát lỗi (thường là access violation trên Windows).
+
+
 #include <stdio.h>
 int main() {
     int *ptr = NULL;
