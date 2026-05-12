dd22d1ea19215ec6aa335fdad8a00654678b5c8c
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:38:50 +0700

    Update Untitled1.c

--- Part02_Mang_Va_Chuoi/Matrix/Ex9/Untitled1.c
+++ Part02_Mang_Va_Chuoi/Matrix/Ex9/Untitled1.c
@@ -1,3 +1,19 @@
+// **9.	Kiểm tra ma trận đối xứng** 
+// ```
+// o	Yêu cầu: Nhập ma trận vuông, kiểm tra xem ma trận có đối xứng qua đường chéo chính không (tức là matrix[i][j] = matrix[j][i]).
+// o	Output mẫu: 
+// Nhap ma tran 3x3:
+// matrix[0][0] = 1
+// matrix[0][1] = 2
+// matrix[0][2] = 3
+// matrix[1][0] = 2
+// matrix[1][1] = 4
+// matrix[1][2] = 5
+// matrix[2][0] = 3
+// matrix[2][1] = 5
+// matrix[2][2] = 6
+// Ma tran doi xung.
+// ```
 #include<stdio.h>
 int main(){
 	int rows, cols;
