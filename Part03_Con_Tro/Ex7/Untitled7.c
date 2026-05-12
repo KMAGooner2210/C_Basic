d218aace69b577c8c0f15744a7266190c1d27d50
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:17:26 +0700

    Update Untitled7.c

--- Part03_Con_Tro/Ex7/Untitled7.c
+++ Part03_Con_Tro/Ex7/Untitled7.c
@@ -1,3 +1,9 @@
+//**Bài 7: Cấp phát bộ nhớ động bằng con trỏ** 
+//Yêu cầu: Viết chương trình cấp phát bộ nhớ động cho một biến int, gán giá trị, in giá trị, sau đó giải phóng bộ nhớ.
+//•	Hướng dẫn: 
+//o	Chạy và kiểm tra kết quả (Giá trị: 50).
+//o	Thử không gọi free(ptr) và giải thích tại sao điều này gây rò rỉ bộ nhớ.
+
 #include<stdio.h>
 int main(){
 	int *ptr = malloc(sizeof(int));
