87bdef640856030e75b17961757f191d4946b0a5
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:14:39 +0700

    Update Untitled3.c

--- Part03_Con_Tro/Ex3/Untitled3.c
+++ Part03_Con_Tro/Ex3/Untitled3.c
@@ -1,3 +1,10 @@
+//**Bài 3: Truy cập mảng bằng con trỏ**
+//•	Yêu cầu: Viết chương trình khai báo mảng int với 5 phần tử, dùng con trỏ để truy cập và in từng phần tử thay vì dùng chỉ số (arr[i]).
+//•	Hướng dẫn: 
+//o	Chạy và kiểm tra đầu ra (1 2 3 4 5).
+//o	Thử thay đổi một phần tử qua con trỏ (ví dụ: *(ptr + 2) = 10;) và in lại mảng.
+
+
 #include<stdio.h>
 int main(){
 	int arr[5]={1, 2, 3, 4, 5};
