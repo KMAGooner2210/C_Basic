e9a949b093970081db358e733cf7dfa490c89136
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:16:05 +0700

    Update Untitled5.c

--- Part03_Con_Tro/Ex5/Untitled5.c
+++ Part03_Con_Tro/Ex5/Untitled5.c
@@ -1,3 +1,9 @@
+//**Bài 5: Tính tổng các phần tử mảng bằng con trỏ**
+//•	Yêu cầu: Viết chương trình khai báo mảng int với 5 phần tử, dùng con trỏ để tính và in tổng các phần tử.
+//•	Hướng dẫn: 
+//o	Chạy và kiểm tra kết quả (Tổng: 15).
+//o	Thử thay đổi giá trị mảng và tính lại tổng.
+
 #include<stdio.h>
 int main(){
 	int arr[5]= {1, 2, 3, 4, 5};
