5a36ed54f3be8ce0e3651d1b2192c16193212c85
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:39:38 +0700

    Update Untitled1.c

--- Part02_Mang_Va_Chuoi/String/Ex1/Untitled1.c
+++ Part02_Mang_Va_Chuoi/String/Ex1/Untitled1.c
@@ -1,3 +1,11 @@
+
+// **1.In chuỗi** 
+// ```
+// o	Yêu cầu: Nhập chuỗi từ bàn phím và in ra màn hình.
+// o	Output mẫu: 
+// Nhap chuoi: hello
+// Chuoi: hello
+// ```
 #include<stdio.h>
 #include <string.h>
 int main(){
