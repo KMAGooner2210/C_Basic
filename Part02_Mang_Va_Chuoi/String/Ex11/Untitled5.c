04629ad945bfffc58db586558b3469a7de546c8b
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:44:09 +0700

    Update Untitled5.c

--- Part02_Mang_Va_Chuoi/String/Ex11/Untitled5.c
+++ Part02_Mang_Va_Chuoi/String/Ex11/Untitled5.c
@@ -1,3 +1,11 @@
+// **11.Tìm chuỗi con** 
+// ```
+// o	Yêu cầu: Nhập chuỗi lớn và chuỗi con, tìm vị trí đầu tiên của chuỗi con trong chuỗi lớn (dùng strstr hoặc thủ công).
+// o	Output mẫu: 
+// Nhap chuoi lon: hello world
+// Nhap chuoi con: world
+// Vi tri: 6
+
 #include<stdio.h>
 #include<string.h>
 
