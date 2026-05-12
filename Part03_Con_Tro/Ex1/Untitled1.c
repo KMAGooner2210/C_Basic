f953059157242d6617701b67a60331e2ebac8e3b
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 13:13:02 +0700

    Update Untitled1.c

--- Part03_Con_Tro/Ex1/Untitled1.c
+++ Part03_Con_Tro/Ex1/Untitled1.c
@@ -1,3 +1,13 @@
+//**Bài 1: In địa chỉ và giá trị của biến qua con trỏ**
+//•	Yêu cầu: Viết chương trình khai báo một biến kiểu int, một con trỏ trỏ tới biến đó, sau đó in: 
+//o	Địa chỉ của biến.
+//o	Giá trị của biến qua con trỏ.
+//o	Địa chỉ của chính con trỏ.
+//•	Hướng dẫn: 
+//o	Chạy chương trình và quan sát đầu ra.
+//o	Thử thay đổi giá trị của x qua *ptr (ví dụ: *ptr = 20;) và in lại x để kiểm tra.
+
+
 #include<stdio.h>
 int main(){
 	int x = 10;
